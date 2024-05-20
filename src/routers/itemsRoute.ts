1aaf5794e6e10efc68d62ebb151cd80e12c2fe50
BotirjonUraimov <botirjon.uraimov.93@gmail.com.com>
2024-05-20 23:26:09 +0900

    Fix: add type and start script in package.json file

--- src/routers/itemsRoute.ts
+++ src/routers/itemsRoute.ts
@@ -2,19 +2,38 @@ import express from "express";
 import axios from "axios";
 import NodeCache from "node-cache";
 
+interface Item {
+  market_hash_name: string;
+  currency: string;
+  suggested_price: number;
+  item_page: string;
+  market_page: string;
+  min_price: number | null;
+  max_price: number | null;
+  mean_price: number | null;
+  median_price?: number | null;
+  created_at: number;
+  updated_at: number;
+  min_price_tradable: number | null;
+  min_price_non_tradable: number | null;
+}
+
 const router = express.Router();
 const cache = new NodeCache({ stdTTL: 600 }); // Cache for 600 seconds (10 minutes)
 
 router.get("/", async (req, res) => {
   try {
     // Check if data is in cache
-    let items = cache.get("items");
+    let items = cache.get<Item[]>("items");
     if (!items) {
       // If no data in cache, fetch from API
       console.log("got from API");
-      const response = await axios.get("https://api.skinport.com/v1/items", {
-        params: { app_id: 730, currency: "EUR" }, // Example parameters
-      });
+      const response = await axios.get<Item[]>(
+        "https://api.skinport.com/v1/items",
+        {
+          params: { app_id: 730, currency: "EUR" }, // Example parameters
+        }
+      );
       items = response.data.map((item: any) => ({
         ...item,
         min_price_tradable: calculateMinPrice(item) ? item.min_price : null,
